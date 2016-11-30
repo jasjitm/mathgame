b7366ab91daf345f7cde3ff8b54b511917e9c549
Jasjit Mann <jmann77@my.bcit.ca>
2016-11-29 19:23:03 -0800

    folder names

--- MathGame.php
+++ MathGame.php
@@ -1,131 +1,137 @@
 <!DOCTYPE html>
 <html>
-   <head>
-      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-      <title>Math Game</title>
-			<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css" media="screen"/>
-   </head>
-   <body>
-      <div class="container">
-         <form action="MathGame.php" method="post" role="form" class="form-horizontal">
-            <div class="row">
-               <div class="col-sm-4 col-sm-offset-4">
-                  <h1>Math Game</h1>
-               </div>
-               <div class="col-sm-4"><a href="Logout.php" class="btn btn-default btn-sm">Logout</a></div>
-            </div>
-            <div class="row">
-               <label class="col-sm-2 col-sm-offset-3"><?php echo $firstNum; ?></label>
-               <label class="col-sm-2"><?php echo $operation; ?></label>
-               <label class="col-sm-2"><?php echo $secondNum; ?></label>
-               <div class="col-sm-3"></div>
-            </div>
-            <input type="hidden" name="firstNum" value="<?php echo $firstNum; ?>">
-            <input type="hidden" name="operation" value="<?php echo $operation; ?>">
-            <input type="hidden" name="secondNum" value="<?php echo $secondNum; ?>">
-            <input type="hidden" name="total" value="<?php echo $total; ?>">
-            <input type="hidden" name="score" value="<?php echo $score; ?>">
-            <div class="form-group">
-               <div class="col-sm-3 col-sm-offset-4">
-                  <input type="text" class="form-control" id="answer" name="answer" placeholder="Enter the answer" size="8">
-               </div>
-               <div class="col-sm-5"></div>
-            </div>
-            <div class="row">
-               <div class="col-sm-3 col-sm-offset-4">
-                  <button type="submit" class="btn btn-primary btn-sm">Submit</button>
-               </div>
-               <div class="col-sm-3"></div>
-            </div>
-         </form>
-         <hr>
-         <div class="row">
-            <div class="col-sm-4 col-sm-offset-4">
-              <?php
-              if($correct == 1) {
-                echo '<span style="color: green; ">Thats right!</span>';
-              } else if($correct == 2) {
-                echo '<span style="color: red; ">Thats wrong!, ' . $firstNum2 . ' ' . $operation2 . ' ' . $secondNum2 . ' is ' . $correctAnswer . '.</span>';
-              } else if ($error) {
-                echo '<span style="color: red;">Please enter a number.</span>';
-              }
-              ?>
-            </div>
-            <div class="col-sm-4"></div>
-         </div>
-         <div class="row">
-            <div class="col-sm-4 col-sm-offset-4">
-              <?php
-              if ($total !== 0) {
-                echo 'Your score: ' . $score . ' / ' . $total;
-              } else {
-                echo 'Your score: 0 / 0';
-              }
-              ?>
-            </div>
-            <div class="col-sm-4"></div>
-         </div>
-      </div>
-    </body>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css" media="screen"/>
+<head>
+  <title>Math Game</title>
+</head>
+<body>
+  <div class="container">
+  <form action="index.php" method="post" role="form" class="form-horizontal">
+  <div class="row">
+    <div class="col-md-4">
+      <h1>Math Game</h1>
+      <?php
+      ini_set('display_errors', 1);
+      ini_set('display_startup_errors', 1);
+      error_reporting(E_ALL);
+      session_start();
+
+      $correctAnswer = NULL;
+      $currentScore = NULL;
+      $userTotal = NULL;
+      $error = NULL;
+
+      $placeHolder = rand(0, 1);
+      $numOne = rand(0, 20);
+      $numTwo = rand(0, 20);
+
+      if(empty($_SESSION['login'])) {
+      	header('Location: login.php');
+      }
+
+      if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $lastNumOne = $_POST['numOne'];
+        $lastNumTwo = $_POST['numTwo'];
+        $lastOperation = $_POST['operation'];
+        $lastcurrentScore = $_POST['currentScore'];
+        $lastUserTotal = $_POST['userTotal'] + 1;
+
+        if (is_numeric($_POST['answer'])) {
+
+          if($lastOperation == '+') {
+            $answer = $lastNumOne + $lastNumTwo;
+          } else {
+            $answer = $lastNumOne - $lastNumTwo;
+          }
+
+          if($_POST['answer'] == $answer) {
+            $correctAnswer = 1;
+            $lastcurrentScore++;
+          } else {
+            $correctAnswer = 2;
+          }
+
+        } else {
+          $error = true;
+        }
+
+        $currentScore = $lastcurrentScore;
+        $userTotal = $lastUserTotal;
+
+      } else {
+        $currentScore = 0;
+        $userTotal = 0;
+      }
+
+      if($placeHolder == 1) {
+        $operation = '+';
+      }
+      else {
+        $operation = '-';
+      }
+      ?>
+  <div class="row">
+    <div class="col-md-3"></div>
+    <label class="col-sm-2 col-sm-offset-3">
+      <?php echo $numOne; ?></label>
+    <label class="col-sm-2">
+      <?php echo $operation; ?></label>
+    <label class="col-sm-2">
+      <?php echo $numTwo; ?></label>
+    </div>
+    <input type="hidden" name="numOne"
+    value="<?php echo $numOne; ?>">
+    <input type="hidden" name="operation"
+     value="<?php echo $operation; ?>">
+    <input type="hidden" name="numTwo"
+    value="<?php echo $numTwo; ?>">
+    <input type="hidden" name="userTotal"
+    value="<?php echo $userTotal; ?>">
+    <input type="hidden" name="currentScore"
+    value="<?php echo $currentScore; ?>">
+  <div class="form-group">
+   <div class="col-md-3">
+    <input type="text" class="form-control" id="answer" name="answer" placeplaceHolder="Enter the answer" size="8">
+   </div>
+  <div class="col-md-5"></div>
+  </div>
+  <div class="row">
+  <div class="col-md-3">
+    <button type="submit" class="btn btn-success">Submit</button>
+  </div>
+  </div>
+  </form>
+  <div class="row">
+    <div class="col-md-4">
+      <?php
+      if($correctAnswer == 1) {
+        echo '<span style="color: green; font-weight: bold;">Thats right!</span>';
+      } else if($correctAnswer == 2) {
+        echo '<span style="color: red; font-weight: bold;">Thats wrong!, '
+         . $lastNumOne . ' ' . $lastOperation . ' ' . $lastNumTwo . ' is ' . $answer . '.</span>';
+      } else if ($error) {
+        echo '<span style="color: red; font-weight: bold;">Please enter a number value.</span>';
+      }
+      ?>
+    </div>
+    <div class="col-md-4">
+    </div>
+    </div>
+  <div class="row">
+    <div class="col-md-4">
+      <?php
+      if ($userTotal !== 0) {
+        echo 'Your score is: ' . $currentScore . ' / ' . $userTotal;
+      } else {
+        echo 'Your score is: 0 / 0';
+      }
+      ?>
+    </div>
+  </div>
+  <div class="col-sm-4"><a href="logout.php" class="btn btn-info">Logout</a></div>
+  </div>
+  </div>
+  </div>
+  </body>
 </html>
-
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-session_start();
-
-if(empty($_SESSION['Login'])) {
-	header('Location: Login.php');
-}
-$correct = NULL;
-$score = NULL;
-$total = NULL;
-$error = NULL;
-
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $firstNum2 = $_POST['firstNumber'];
-  $secondNum2 = $_POST['secondNumber'];
-  $operation2 = $_POST['operationPeformed'];
-  $score2 = $_POST['score'];
-  $total2 = $_POST['total'] + 1;
-
-  if (is_numeric($_POST['answer'])) {
-
-    if($operation2 == '+') {
-      $answer = $firstNum2 + $secondNum2;
-    } else {
-      $answer = $firstNum2 - $secondNum2;
-    }
-
-    if($_POST['answer'] == $answer) {
-      $correct = 1;
-      $score2++;
-    } else {
-      $correct = 2;
-    }
-
-  } else {
-    $error = true;
-  }
-
-  $score = $score2;
-  $total = $total2;
-
-} else {
-  $score = 0;
-  $total = 0;
-}
-
-$placeHolder = rand(0, 1);
-$firstNum2 = rand(0, 20);
-$secondNum2 = rand(0, 20);
-
-if($placeHolder == 1) {
-  $operation = '+';
-}
-else {
-  $operation = '-';
-}
-
-?>
--- index.php
+++ index.php
@@ -1,137 +1,54 @@
+<?php
+session_start();
+if(isset($_SESSION['login']) && $_SESSION['login'] == 'yes') {
+	header('Location: index.php');
+}
+?>
 <!DOCTYPE html>
 <html>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css" media="screen"/>
-<head>
-  <title>Math Game</title>
-</head>
-<body>
-  <div class="container">
-  <form action="index.php" method="post" role="form" class="form-horizontal">
-  <div class="row">
-    <div class="col-md-4">
-      <h1>Math Game</h1>
-      <?php
-      ini_set('display_errors', 1);
-      ini_set('display_startup_errors', 1);
-      error_reporting(E_ALL);
-      session_start();
-
-      $correctAnswer = NULL;
-      $currentScore = NULL;
-      $userTotal = NULL;
-      $error = NULL;
-
-      $placeHolder = rand(0, 1);
-      $numOne = rand(0, 20);
-      $numTwo = rand(0, 20);
-
-      if(empty($_SESSION['login'])) {
-      	header('Location: login.php');
-      }
-
-      if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $lastNumOne = $_POST['numOne'];
-        $lastNumTwo = $_POST['numTwo'];
-        $lastOperation = $_POST['operation'];
-        $lastcurrentScore = $_POST['currentScore'];
-        $lastUserTotal = $_POST['userTotal'] + 1;
-
-        if (is_numeric($_POST['answer'])) {
-
-          if($lastOperation == '+') {
-            $answer = $lastNumOne + $lastNumTwo;
-          } else {
-            $answer = $lastNumOne - $lastNumTwo;
-          }
-
-          if($_POST['answer'] == $answer) {
-            $correctAnswer = 1;
-            $lastcurrentScore++;
-          } else {
-            $correctAnswer = 2;
-          }
-
-        } else {
-          $error = true;
-        }
-
-        $currentScore = $lastcurrentScore;
-        $userTotal = $lastUserTotal;
-
-      } else {
-        $currentScore = 0;
-        $userTotal = 0;
-      }
-
-      if($placeHolder == 1) {
-        $operation = '+';
-      }
-      else {
-        $operation = '-';
-      }
-      ?>
-  <div class="row">
-    <div class="col-md-3"></div>
-    <label class="col-sm-2 col-sm-offset-3">
-      <?php echo $numOne; ?></label>
-    <label class="col-sm-2">
-      <?php echo $operation; ?></label>
-    <label class="col-sm-2">
-      <?php echo $numTwo; ?></label>
+	<head>
+    <title>Math Game</title>
+  </head>
+  <body>
+  	<div class="container">
+    <div class="row">
+    <div class="col-md-10">
+      <h3>Login to enjoy the math game.</h3>
     </div>
-    <input type="hidden" name="numOne"
-    value="<?php echo $numOne; ?>">
-    <input type="hidden" name="operation"
-     value="<?php echo $operation; ?>">
-    <input type="hidden" name="numTwo"
-    value="<?php echo $numTwo; ?>">
-    <input type="hidden" name="userTotal"
-    value="<?php echo $userTotal; ?>">
-    <input type="hidden" name="currentScore"
-    value="<?php echo $currentScore; ?>">
-  <div class="form-group">
-   <div class="col-md-3">
-    <input type="text" class="form-control" id="answer" name="answer" placeplaceHolder="Enter the answer" size="8">
-   </div>
-  <div class="col-md-5"></div>
-  </div>
-  <div class="row">
-  <div class="col-md-3">
-    <button type="submit" class="btn btn-success">Submit</button>
-  </div>
-  </div>
-  </form>
-  <div class="row">
-    <div class="col-md-4">
-      <?php
-      if($correctAnswer == 1) {
-        echo '<span style="color: green; font-weight: bold;">Thats right!</span>';
-      } else if($correctAnswer == 2) {
-        echo '<span style="color: red; font-weight: bold;">Thats wrong!, '
-         . $lastNumOne . ' ' . $lastOperation . ' ' . $lastNumTwo . ' is ' . $answer . '.</span>';
-      } else if ($error) {
-        echo '<span style="color: red; font-weight: bold;">Please enter a number value.</span>';
-      }
-      ?>
+    <div class="col-md-1"></div>
     </div>
-    <div class="col-md-4">
+    <form action="authenticate.php" method="post" role="form" class="form-horizontal">
+		<div class="form-group">
+    	<div class="col-md-4">Enter your email: </div>
+    	<div class="col-md-3">
+      <input type="text" class="form-control" id="email" name="email" placeholder="Email" size="6">
+    	</div>
+    	<div class="col-md-5"></div>
+    	</div>
+    <div class="form-group">
+      <div class="col-md-4">Enter your password: </div>
+      <div class="col-md-3">
+      <input type="text" class="form-control" id="password" name="password" placeholder="Password" size="6">
+      </div>
+      <div class="col-md-5"></div>
+      </div>
+    <div class="row">
+      <div class="col-md-3">
+        <button type="submit" class="btn btn-info">Login</button>
+      </div>
     </div>
+    </form>
+    <div class="row">
+			<?php
+			if(isset($_GET['msg'])) {
+			$msg = $_GET['msg'];
+			urldecode($msg);
+			echo '<p class="col-md-3">' . $msg . '</p>';
+			}
+			?>
+		</div>
     </div>
-  <div class="row">
-    <div class="col-md-4">
-      <?php
-      if ($userTotal !== 0) {
-        echo 'Your score is: ' . $currentScore . ' / ' . $userTotal;
-      } else {
-        echo 'Your score is: 0 / 0';
-      }
-      ?>
-    </div>
-  </div>
-  <div class="col-sm-4"><a href="logout.php" class="btn btn-info">Logout</a></div>
-  </div>
-  </div>
-  </div>
   </body>
 </html>
