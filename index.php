1e72e98cd87887d9105bea406d13fb7d54678dc6
Jasjit Mann <jmann77@my.bcit.ca>
2016-11-29 17:32:40 -0800

    fixed errors

--- index.php
+++ index.php
@@ -12,86 +12,92 @@
     <div class="col-md-4">
       <h1>Math Game</h1>
       <?php
+
       ini_set('display_errors', 1);
       ini_set('display_startup_errors', 1);
       error_reporting(E_ALL);
       session_start();
 
-      if(empty($_SESSION['login'])) {
-        header('Location: login.php');
-      }
-
       $correctAnswer = NULL;
-      $userScore = NULL;
+      $currentScore = NULL;
       $userTotal = NULL;
       $error = NULL;
 
-      if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $numberOne = $_POST['firstNumber'];
-        $numberTwo = $_POST['secondNumber'];
-        $operationPeformed = $_POST['operation'];
-        $userScore2 = $_POST['userScore'];
-        $userTotal2 = $_POST['userTotal'] + 1;
+      $placeHolder = rand(0, 1);
+      $numOne = rand(0, 20);
+      $numTwo = rand(0, 20);
 
-      if (is_numeric($_POST['answer'])) {
-
-        if($_POST['answer'] == $correctAnswer) {
-          $correctAnswer = 1;
-          $userScore2++;
-        } else {
-          $correctAnswer = 2;
-        }
-
-      } else {
-        $error = true;
+      if(empty($_SESSION['login'])) {
+      	header('Location: login.php');
       }
 
-        if($operationPeformed == '+') {
-          $correctAnswer = $numberOne + $numberTwo;
+      if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $lastNumOne = $_POST['numOne'];
+        $lastNumTwo = $_POST['numTwo'];
+        $lastOperation = $_POST['operation'];
+        $lastcurrentScore = $_POST['currentScore'];
+        $lastUserTotal = $_POST['userTotal'] + 1;
+
+        if (is_numeric($_POST['answer'])) {
+
+          if($lastOperation == '+') {
+            $answer = $lastNumOne + $lastNumTwo;
+          } else {
+            $answer = $lastNumOne - $lastNumTwo;
+          }
+
+          if($_POST['answer'] == $answer) {
+            $correctAnswer = 1;
+            $lastcurrentScore++;
+          } else {
+            $correctAnswer = 2;
+          }
+
         } else {
-          $correctAnswer = $numberOne - $numberTwo;
+          $error = true;
         }
 
-      $userScore = $userScore2;
-      $userTotal = $userTotal2;
+        $currentScore = $lastcurrentScore;
+        $userTotal = $lastUserTotal;
 
-    } else {
-      $userScore = 0;
-      $userTotal = 0;
-    }
+      } else {
+        $currentScore = 0;
+        $userTotal = 0;
+      }
 
-    $placeHolder = rand(0, 1);
-    $firstNumber = rand(0, 20);
-    $secondNumber = rand(0, 20);
-    if($placeHolder == 1) {
-      $operation = '+';
-    }
-    else {
-      $operation = '-';
-    }
-    ?>
+      if($placeHolder == 1) {
+        $operation = '+';
+      }
+      else {
+        $operation = '-';
+      }
+
+      ?>
     </div>
     <div class="col-sm-4"><a href="logout.php" class="btn btn-default btn-sm">Logout</a></div>
     </div>
   <div class="row">
     <div class="col-md-3"></div>
-      <label class="col-md-2"><?php echo $firstNumber; ?></label>
-      <label class="col-md-2"><?php echo $operation; ?></label>
-      <label class="col-md-2"><?php echo $secondNumber; ?></label>
+    <label class="col-sm-2 col-sm-offset-3">
+      <?php echo $numOne; ?></label>
+    <label class="col-sm-2">
+      <?php echo $operation; ?></label>
+    <label class="col-sm-2">
+      <?php echo $numTwo; ?></label>
     </div>
-      <input type="hidden" name="firstNumber"
-      value="<?php echo $firstNumber; ?>">
-      <input type="hidden" name="operation"
-      value="<?php echo $operation; ?>">
-      <input type="hidden" name="secondNumber"
-      value="<?php echo $secondNumber; ?>">
-      <input type="hidden" name="userScore"
-      value="<?php echo $userScore; ?>">
-      <input type="hidden" name="userTotal"
-      value="<?php echo $userTotal; ?>"
+    <input type="hidden" name="numOne"
+    value="<?php echo $numOne; ?>">
+    <input type="hidden" name="operation"
+     value="<?php echo $operation; ?>">
+    <input type="hidden" name="numTwo"
+    value="<?php echo $numTwo; ?>">
+    <input type="hidden" name="userTotal"
+    value="<?php echo $userTotal; ?>">
+    <input type="hidden" name="currentScore"
+    value="<?php echo $currentScore; ?>">
   <div class="form-group">
    <div class="col-md-3">
-    <input type="text" class="form-control" id="answer" name="answer" placeholder="Enter the answer" size="8">
+    <input type="text" class="form-control" id="answer" name="answer" placeplaceHolder="Enter the answer" size="8">
    </div>
   <div class="col-md-5"></div>
   </div>
@@ -104,11 +110,10 @@
   <div class="row">
     <div class="col-md-4">
       <?php
-      if($answer == 1) {
-        echo '<span style="color: green; font-weight: bold;">Thats correct!</span>';
-      } else if($answer == 2) {
-        echo '<span style="color: red; font-weight: bold;">Thats incorrect!, '
-        . $numberOne . ' ' . $operationPeformed . ' ' . $numberTwo . ' = ' . $correctAnswer . '.</span>';
+      if($correctAnswer == 1) {
+        echo '<span style="color: green; font-weight: bold;">Thats right!</span>';
+      } else if($correctAnswer == 2) {
+        echo '<span style="color: red; font-weight: bold;">Thats wrong!, ' . $lastNumOne . ' ' . $lastOperation . ' ' . $lastNumTwo . ' is ' . $answer . '.</span>';
       } else if ($error) {
         echo '<span style="color: red; font-weight: bold;">Please enter a number value.</span>';
       }
@@ -121,9 +126,9 @@
     <div class="col-md-4">
       <?php
       if ($userTotal !== 0) {
-        echo 'Your score is: ' . $userTotal . ' / ' . $userScore;
+        echo 'currentScore: ' . $currentScore . ' / ' . $userTotal;
       } else {
-        echo 'Your score is: 0 / 0';
+        echo 'currentScore: 0 / 0';
       }
       ?>
     </div>
